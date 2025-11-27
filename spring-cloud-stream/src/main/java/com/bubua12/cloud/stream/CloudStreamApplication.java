c355fc8d5832241c7c40caf2a81378c598cfaca2
wangxl <wangty334752@gmail.com>
2025-11-27 19:38:22 +0800

    feat：添加stream数据流处理模块

---
+++ spring-cloud-stream/src/main/java/com/bubua12/cloud/stream/CloudStreamApplication.java
@@ -0,0 +1,17 @@
+package com.bubua12.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author bubua12
+ * @since 2025/11/27 14:50
+ */
+@SpringBootApplication
+public class CloudStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamApplication.class, args);
+    }
+}
